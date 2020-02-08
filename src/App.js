a97df38371026e4816f2fc7ecc05852b00ece044
dawittilahunm07 <55716289+dawittilahunm07@users.noreply.github.com>
2020-02-08 07:37:01 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+     <div class="box" onclick="clickTest()"></div>
+<button class="btn btn-info" id="button">Click me</button>
+<p>Hello</p>
+<button id="run">run</button>
+    </div>
+  );
+}
